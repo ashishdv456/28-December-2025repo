4e34ddfe8592e6b6a43cdc9b646af420e943b702
ashishdv456 <ashishdv456@gmail.com>
2025-12-28 16:30:12 +0530

    keyvoult

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.54.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "3d1ce785-6c4d-4565-bb5e-3b46939260a6"
+}
